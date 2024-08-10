ff2a13e2fe9ed4a1822a290630b7806e004b876e
landonbr <landonbrown0797@gmail.com>
2024-08-10 17:22:44 -0400

    Refactor
    Made code more readable with optional debug output
    that can be enabled by changing the DEBUG_OUTPUT
    gloabl define to 0 or 1.

--- bootloader.c
+++ bootloader.c
@@ -1,7 +1,8 @@
-// This is basically a simualtion in parsing an SREC file 
-// that would load your program
+// This is basically a simualtion in parsing an SREC file that would load your program.
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEBUG_OUTPUT 1
 
 FILE *fp;
 void init_file() {
@@ -14,6 +15,7 @@ void init_file() {
     }
 }
 
+// Simulate functions for using the UART
 #define uart_init() init_file()
 #define uart_send(...)  putchar(__VA_ARGS__)
 #define hexstring(fmt, ...) printf(fmt, __VA_ARGS__)
@@ -24,7 +26,7 @@ int uart_recv() {
 }
 
 void PUT8(unsigned int address, unsigned char data) {
-    printf("Writing %02X to %08X\n", data, address); 
+    printf("Writing 0x%02X to 0x%08X\n", data, address); 
 }
 
 // byte to nibblet conversion
@@ -41,9 +43,10 @@ typedef struct srec_decode {
     unsigned int checksum;
 } srec_decode;
 
-int main(int argc, char**argv) 
+// 0x0D - '\r'
+// 0x0A - '\n'
+int main(void) 
 { 
-    int cnt = 0;
     uart_init();
 
     uart_send(0x0D);
@@ -62,10 +65,14 @@ int main(int argc, char**argv)
     uart_send('C');
     uart_send(0x0D);
     uart_send(0x0A);
+    uart_send(0x0D);
+    uart_send(0x0A);
 
-    unsigned int entry = 0x8000;
-    //while (1) {
+    unsigned int entry_start = 0x8000; // entry point of program to load
+
+    // Note, we would usually sit in this loop forever while the program runs - we will return for sim purposes
     while ((ra = uart_recv())) {
+        // Begining of SREC line to process
         if (ra == 'S') {
             sbug.type[0] = 'S';
             sbug.checksum = 0;
@@ -78,7 +85,6 @@ int main(int argc, char**argv)
             continue;
         }
 
-
         switch (state)
         {
             case 0:
@@ -90,21 +96,15 @@ int main(int argc, char**argv)
                 {
                     case '0':
                         sbug.type[1] = '0';
-                        //hexstring(sbug.type[1]);
-                        //state = 0;
                         state++;
                         break;
                     case '3':
                         sbug.type[1] = '3';
-                        //hexstring(sbug.type[1]);
                         state++;
                         break;
                     case '7':
                         sbug.type[1] = '7';
-                        hexstring("%c\n", sbug.type[1]);
-                        hexstring("%08X\n",0xDEADBEEF);
-                        hexstring("%X\n", sbug.checksum);
-                        //state = 0;
+                        hexstring("%08X\n",0xDEADDEAD);
                         state++;
                         break;
                     default:
@@ -114,65 +114,78 @@ int main(int argc, char**argv)
                 }
                 break;
             case 2:
+#if DEBUG_OUTPUT
+                printf("================================\n");
+                printf("Processing Byte Count section...\n");
+#endif
                 sbug.count = ctonib(ra);
                 state++;
                 break;
             case 3:
                 sbug.count <<= 4;
                 sbug.count |= ctonib(ra);
-                hexstring("-%d-\n",sbug.count);
+#if DEBUG_OUTPUT
+                hexstring("Byte Count: 0x%X\n", sbug.count);
+#endif
                 sbug.checksum += (sbug.count&0xFF);
                 sbug.addr=0;
-                data_count = sbug.count*2; // TODO: maybe correct - need to figure out how the data count reaches the end to the check sum but doesnt write to address 
+                data_count = sbug.count*2; // number of hex digits to process
                 addr_count = 0;
-                hexstring("*%d*\n",data_count);
-                printf("%d++\n", sbug.checksum);
+#if DEBUG_OUTPUT
+                printf("Processing Address section...\n");
+                hexstring("Hexdigits to Process: %d\n", data_count);
+#endif
                 state++;
                 break;
             case 4:
-                if (addr_count == 8) {
+                if (addr_count == 8) { // Finished processing address section
                     sbug.data = ctonib(ra);
                     data_count--;
                     state++;
-                    //printf("x%X\n", sbug.addr);
-                    printf("================\n");
                 } else {
                     sbug.addr <<= 4;
                     sbug.addr |= ctonib(ra);
-                    hexstring("BA: x%0X data_count %d\n", ctonib(ra), data_count);
-                    // TODO: fix how checksum accumulates. Should accummulate byte by byte but 
-                    if (addr_count % 2) { // accumulate checksum on a hexdigit pair basis
+                    // accumulate checksum on a hexdigit pair basis
+                    if (addr_count % 2) { 
                         sbug.checksum += (sbug.addr&0xFF);
-                        printf("x%X++ x%X\n", sbug.checksum, sbug.addr&0xFF);
+#if DEBUG_OUTPUT
+                        printf("checksum: 0x%X address:  0x%X\n", sbug.checksum, sbug.addr&0xFF);
+#endif
                     }
                     addr_count++;
+#if DEBUG_OUTPUT
+                    if (addr_count == 8) {
+                        printf("Processing Data section...\n");
+                        hexstring("Hexdigits to Process: %d\n", data_count-1);
+                    }
+#endif
                     data_count--;
                 }
                 break;
             case 5:
-                if (data_count == 1) {
-                    // Indicates end of current line of srec line (i.e we are at the checksum byte)
+                if (data_count == 1) { // Indicates end of current line of srec line (i.e we are at the checksum byte)
+                                       // Don't load the checksum byte into the program (i.e dont PUT8())
                     sbug.checksum = 0xFF - (sbug.checksum & 0xFF);
-                    hexstring("ADDRESS: x%08X______________x%02X\n", sbug.addr, sbug.checksum&0xFF);
+#if DEBUG_OUTPUT
+                    printf("Calculating Final Checksum...\n");
+                    hexstring("ADDRESS: 0x%08X ========== 0x%02X\n\n", sbug.addr, sbug.checksum&0xFF);
+#endif
                     state = 0;
-                    //data_count = 0;
-                    //addr_count = 0;
-                    //return 0;
                 } else {
                     sbug.data <<= 4;
                     sbug.data |= ctonib(ra);
                     if (data_count > 0) {
-                        if (data_count % 2) {
+                        if (data_count % 2) { // accumulate checksum on a hexdigit pair basis
                             sbug.checksum += (sbug.data&0xFF);
-                            printf("x%X++ x%X\n", sbug.checksum, sbug.data&0xFF);
-                            //cnt++;
-                            //if (cnt == 32) { return 0; }
+#if DEBUG_OUTPUT
+                            hexstring("checksum: 0x%X data: 0x%X\n", sbug.checksum, sbug.data&0xFF);
+#endif
                             i++;
                         }
-                        //printf("data_count: %d\n", data_count);
-                        //PUT8(sbug.addr, sbug.data);
+#ifndef DEBUG_OUTPUT
+                        PUT8(sbug.addr, sbug.data);
+#endif
                     }
-                    //PUT32(sbug.addr, sbug.data);
                     sbug.addr++;
                     data_count--;
                     state = 4;
@@ -183,5 +196,7 @@ int main(int argc, char**argv)
                 break;
         }
     }
+    hexstring("Loading program at entry point 0x%08X...\n", entry_start);
+
     return 0;
 }
